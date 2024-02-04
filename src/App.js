6cea8878e4049b45d46dacd27c5c5fc380ac94f4
julioscasme <jdk.jdcm99@gmail.com>
2024-02-04 05:17:03 +0100

    Proyecto testimonio finalizado

--- src/App.js
+++ src/App.js
@@ -23,6 +23,16 @@ function App() {
           empresa = "Amazon"
           testimonio = "Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."
           /> 
+
+        <Testimonio 
+          nombre = "Sara Chima "
+          pais = "Singapur"
+          imagen = "shaw"
+          cargo = "Ingeniero de Software"
+          empresa = "Amazon"
+          testimonio = "Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."
+          /> 
+
       </div>
     </div>
   );
