49295c6640aa4a8988a5d67f4549ec6be4f9f45d
julioscasme <jdk.jdcm99@gmail.com>
2024-02-04 05:23:51 +0100

    Proyecto de testimonios finalziado

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,19 +1,27 @@
-import React from "react";
-import "../style/Testimonio.css";
+  import React from "react";
+  import "../style/Testimonio.css";
 
-function Testimonio(props){
-  return(
-    <div className="contenedor-testimonio">
-      <img className="imagen-testimonio"
-           src={require(`../img/testimonio-${props.imagen}.png`)}
-           alt = 'imagen de Emma'/>
-      
-      <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
-        <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
-        <p className="texto-testimonio">"{props.testimonio}"</p>
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  function Testimonio(props){
+
+    const imagenConMayuscula = capitalizeFirstLetter(props.imagen);
+
+    return(
+
+      <div className="contenedor-testimonio">
+        <img className="imagen-testimonio"
+            src={require(`../img/testimonio-${props.imagen}.png`)}
+            alt = {`imagen de ${imagenConMayuscula}`}  />
+        
+        <div className="contenedor-texto-testimonio">
+          <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
+          <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+          <p className="texto-testimonio">"{props.testimonio}"</p>
+        </div>
       </div>
-    </div>
-  );
-}
-export default Testimonio;
+    );
+  }
+  export default Testimonio;
